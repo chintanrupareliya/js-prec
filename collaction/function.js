a315b33fc0a5e4ca6a2cbba1cf57961e007f245a
Chintan Rupareliya <90739955+chintanrupareliya@users.noreply.github.com>
2023-09-17 08:14:49 +0000

    some change

--- collaction/function.js
+++ collaction/function.js
@@ -1,13 +1,13 @@
-console.log(3+"3");
+// console.log(3+"3");
 //javascript datatype conversion confusion
 
 //Number
-let r="3";
-console.log(r);
-console.log(typeof r);
-let c=Number(r);
-console.log(c);
-console.log(typeof c);
+// let r="3";
+// console.log(r);
+// console.log(typeof r);
+// let c=Number(r);
+// console.log(c);
+// console.log(typeof c);
 // "33" --> 33 number
 //"33dfs" --> NaN Not a Number
 // Null --> 0
@@ -15,13 +15,31 @@ console.log(typeof c);
 
 //String
 
-let a=null;
-console.log(a);
-console.log(typeof a);
-let b=String(a);
-console.log(b);
-console.log(typeof b);
+// let a=null;
+// console.log(a);
+// console.log(typeof a);
+// let b=String(a);
+// console.log(b);
+// console.log(typeof b);
 // "33"-->"33"
 // 33-->"33"
 //true -->"true";
 //null(object)-->"null"(string)
+
+//as it Boolean
+
+//why string to number conversion is confusing
+// console.log(3+"3");
+// console.log("3"+"3");
+// console.log("3"+3);
+// console.log(2+3+"5");
+// console.log(3-"3");
+// console.log("3"-3);
+// console.log("3"-3+"5");
+// console.log("3"+3-5);
+// let l=4
+// console.log(++l);
+// console.log(l);
+// console.log(l++);
+// console.log(l);
+

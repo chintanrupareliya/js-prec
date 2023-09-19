118a7d091e8b18fa96ef75034b78b14dc1a7f696
Chintan Rupareliya <90739955+chintanrupareliya@users.noreply.github.com>
2023-09-19 05:50:46 +0000

    math and number

---
+++ collaction/math_number.js
@@ -0,0 +1,18 @@
+const n=new Number(4);
+console.log(n);
+let x=9.43;
+console.log(x.toPrecision);
+console.log(x.toFixed(2));
+console.log(x.toExponential(2));
+console.log(x.toString());
+
+//Math object
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(4.5));
+
+console.log(Math.ceil(4.1));
+
+console.log(Math.floor(4.9));
+console.log(Math.abs(-4.9));
+console.log(Math.random());
